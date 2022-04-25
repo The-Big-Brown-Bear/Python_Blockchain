3c7ad408939a6cee3635e2576f0e2bb167f0186c
bboden5000 <benjamin@2canex.com>
2022-04-25 10:37:18 -0600

    Update Blockchain.py

--- Blockchain.py
+++ Blockchain.py
@@ -4,8 +4,8 @@
 # Class: (INFO 1200)
 # Section: (X01)
 # Professor: (Crandall)
-# Date: 18/03/12022
-# Project #: MO7
+# Date: 25/04/12022
+# Project #: Final
 ''' I declare that the source code contained in this assignment was written solely by me.
     I understand that copying any source code, in whole or in part, 
     constitutes cheating, and that I will receive a zero on this project
@@ -13,7 +13,7 @@
 
 
 # Name: main () function
-# Description: main program function
+# Job: main program function
 # Peramiters: none
 def main ():
     pass
